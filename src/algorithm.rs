04e9d7736edd4b47d7300c56c84215c1948279b0
Abiria <abiria.dev@gmail.com>
2023-04-07 07:46:31 +0900

    refactor: correct typo, clean up module

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -1,28 +1,33 @@
 use crate::Outcome;
 
 #[inline]
-fn delta(s: Outcome, e: f64, k: f64) -> f64 { k * (s - e) }
+fn delta(o: Outcome, e: f64, k: f64) -> f64 { k * (o - e) }
 
 #[inline]
-fn exa(ra: f64, rb: f64) -> f64 { 1. / (1. + 10f64.powf((rb - ra) / 400.)) }
+fn ex(ra: f64, rb: f64) -> f64 { 1. / (1. + 10f64.powf((rb - ra) / 400.)) }
 
 #[inline]
-fn ex(ra: f64, rb: f64) -> (f64, f64) {
-	let ea = exa(ra, rb);
+fn expect(ra: f64, rb: f64) -> (f64, f64) {
+	let ea = ex(ra, rb);
 
 	(ea, 1. - ea)
 }
 
 #[inline]
-pub(crate) fn elo_a(ra: f64, rb: f64, sa: Outcome, k: f64) -> f64 {
-	ra + delta(sa, exa(ra, rb), k)
+pub(crate) fn elo_a(ra: f64, rb: f64, o: Outcome, k: f64) -> f64 {
+	ra + delta(o, ex(ra, rb), k)
 }
 
-pub fn elo(ra: f64, rb: f64, sa: Outcome, k: f64) -> (f64, f64) {
-	let (ea, eb) = ex(ra, rb);
+pub fn elo(
+	rating_a: f64,
+	rating_b: f64,
+	outcome: Outcome,
+	k: f64,
+) -> (f64, f64) {
+	let (ea, eb) = expect(rating_a, rating_b);
 
 	(
-		ra + delta(sa, ea, k),
-		rb + delta(!sa, eb, k),
+		rating_a + delta(outcome, ea, k),
+		rating_b + delta(!outcome, eb, k),
 	)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,20 @@ pub use outcome::*;
 pub struct Player(f64);
 
 impl Player {
-	pub fn new(ra: f64) -> Self { Self(ra) }
+	pub fn new(rating: f64) -> Self { Self(rating) }
 
 	pub fn rating(&self) -> f64 { self.0 }
 
-	pub fn update_rating(&mut self, rb: f64, sa: Outcome, k: f64) {
-		self.0 = elo_a(self.0, rb, sa, k)
+	pub fn update_rating(
+		&mut self,
+		opponent_rating: f64,
+		outcome: Outcome,
+		k: f64,
+	) {
+		self.0 = elo_a(self.0, opponent_rating, outcome, k)
 	}
 
-	fn set_rating(&mut self, new_rateing: f64) { self.0 = new_rateing; }
+	fn set_rating(&mut self, rating: f64) { self.0 = rating; }
 }
 
 pub struct Ratings {
@@ -49,9 +54,9 @@ impl Ratings {
 		};
 		unsafe {
 			let (ar, br) = ((*a).rating(), (*b).rating());
-			let (ra, rb) = elo(ar, br, result, self.k);
-			(*a).set_rating(ra);
-			(*b).set_rating(rb);
+			let (ar_new, br_new) = elo(ar, br, result, self.k);
+			(*a).set_rating(ar_new);
+			(*b).set_rating(br_new);
 		}
 		Ok(())
 	}
