ce5dee3002b0c22c82a373b06bcea69e489f92ed
Abiria <abiria.dev@gmail.com>
2023-04-07 02:58:34 +0900

    feat: add Ratings struct

---
+++ examples/ratings.rs
@@ -0,0 +1,15 @@
+use elo_rs::{Player, Ratings};
+
+fn main() {
+	let mut rating = Ratings::new(
+		vec![Player::new(123.), Player::new(456.)],
+		32.,
+	);
+
+	rating.r#match(0, 1, 0.).unwrap();
+
+	let players = rating.into_vec();
+
+	assert_eq!(players[0].rating(), 118.89736249706061);
+	assert_eq!(players[1].rating(), 460.1026375029394);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -26,3 +26,40 @@ impl Player {
 		self.0 = elo(self.0, rb, sa, 1.0 - sa, k).0
 	}
 }
+
+pub struct Ratings {
+	players: Vec<Player>,
+	k: f64,
+}
+
+impl Ratings {
+	pub fn new(players: Vec<Player>, k: f64) -> Self { Self { players, k } }
+
+	pub fn into_vec(self) -> Vec<Player> { self.players }
+
+	pub fn r#match(
+		&mut self,
+		a: usize,
+		b: usize,
+		result: f64,
+	) -> Result<(), ()> {
+		if a == b {
+			return Err(());
+		}
+		let (Some(a), Some(b)) = (
+			self.players
+				.get_mut(a)
+				.map(|t| t as *mut Player),
+			self.players
+				.get_mut(b)
+				.map(|t| t as *mut Player),
+		) else {
+			return Err(());
+		};
+		unsafe {
+			(*a).update_rating((*b).rating(), result, self.k);
+			(*b).update_rating((*a).rating(), 1. - result, self.k);
+		}
+		Ok(())
+	}
+}
