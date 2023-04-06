d0da79c47a831ba133f62d6707815cc03da5bad7
Abiria <abiria.dev@gmail.com>
2023-04-07 07:05:14 +0900

    refactor: extract outcome module

---
+++ src/outcome.rs
@@ -0,0 +1,48 @@
+#[derive(Clone, Copy)]
+pub enum Outcome {
+	Win,
+	Draw,
+	Loss,
+}
+
+use std::ops::{Mul, Not, Sub};
+
+pub use Outcome::*;
+
+impl From<Outcome> for f64 {
+	#[inline]
+	fn from(value: Outcome) -> Self {
+		match value {
+			Win => 1.,
+			Draw => 0.5,
+			Loss => 0.,
+		}
+	}
+}
+
+impl Sub<f64> for Outcome {
+	type Output = f64;
+
+	fn sub(self, rhs: f64) -> Self::Output { f64::from(self) - rhs }
+}
+
+impl Mul<f64> for Outcome {
+	type Output = f64;
+
+	#[inline]
+	fn mul(self, rhs: f64) -> Self::Output { f64::from(self) * rhs }
+}
+
+impl Not for Outcome {
+	type Output = Self;
+
+	#[inline]
+	fn not(self) -> Self::Output {
+		// 1. - f64::from(self)
+		match self {
+			Win => Loss,
+			Draw => Draw,
+			Loss => Win,
+		}
+	}
+}
