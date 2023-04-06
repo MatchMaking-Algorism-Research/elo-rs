5f8c1a15cec7b52cf1927cf6467078360c672d02
Abiria <abiria.dev@gmail.com>
2023-04-07 07:36:59 +0900

    feat: extract minimal operations

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -1,11 +1,23 @@
 use crate::Outcome;
 
+#[inline]
 pub(crate) fn delta(s: Outcome, e: f64, k: f64) -> f64 { k * (s - e) }
 
+#[inline]
+pub(crate) fn exa(ra: f64, rb: f64) -> f64 {
+	1. / (1. + 10f64.powf((rb - ra) / 400.))
+}
+
+#[inline]
 pub(crate) fn ex(ra: f64, rb: f64) -> (f64, f64) {
-	let ea = 1. / (1. + 10f64.powf((rb - ra) / 400.));
+	let ea = exa(ra, rb);
 
 	(ea, 1. - ea)
+}
+
+#[inline]
+pub(crate) fn elo_a(ra: f64, rb: f64, sa: Outcome, k: f64) -> f64 {
+	ra + delta(sa, exa(ra, rb), k)
 }
 
 pub fn elo(ra: f64, rb: f64, sa: Outcome, k: f64) -> (f64, f64) {
