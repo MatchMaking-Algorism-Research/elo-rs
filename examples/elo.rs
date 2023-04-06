7c85679a0690c504a488a19667147207ab5801b6
Abiria <abiria.dev@gmail.com>
2023-04-07 07:52:25 +0900

    docs: update example

--- examples/elo.rs
+++ examples/elo.rs
@@ -4,7 +4,7 @@ fn main() {
 	let a = 123.;
 	let b = 456.;
 
-	let (a, b) = elo(a, b, Loss, 32.);
+	let (a, b) = elo(a, b, Loss);
 
 	assert_eq!(a, 118.89736249706061);
 	assert_eq!(b, 460.1026375029394);
