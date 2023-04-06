bae5c9b309c11184c5253ead8cc87e8c01272a54
Abiria <abiria.dev@gmail.com>
2023-04-07 01:07:13 +0900

    feat: implement basics

--- examples/elo.rs
+++ examples/elo.rs
@@ -1,3 +1,11 @@
+use elo_rs::elo;
+
 fn main() {
-	todo!();
+	let a = 123.;
+	let b = 456.;
+
+	let (a, b) = elo(a, b, 0., 1., 32.);
+
+	assert_eq!(a, -217.59553440106316);
+	assert_eq!(b, 217.59553440106316);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
-//
+fn delta(s: f64, e: f64, k: f64) -> f64 { k * (s - e) }
+
+fn ex(ra: f64, rb: f64) -> (f64, f64) {
+	let ea = 10f64.powf((rb - ra) / 400.);
+
+	(ea, 1. - ea)
+}
+
+pub fn elo(ra: f64, rb: f64, sa: f64, sb: f64, k: f64) -> (f64, f64) {
+	let (ea, eb) = ex(ra, rb);
+
+	(delta(sa, ea, k), delta(sb, eb, k))
+}
