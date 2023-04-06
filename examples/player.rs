01dcef200d53be67b473e119302dc4a03e93949a
Abiria <abiria.dev@gmail.com>
2023-04-07 07:52:27 +0900

    docs: update example

--- examples/player.rs
+++ examples/player.rs
@@ -3,7 +3,7 @@ use elo_rs::{Loss, Player};
 fn main() {
 	let mut player = Player::new(123.);
 
-	player.update_rating(456., Loss, 32.);
+	player.update_rating(456., Loss);
 
 	assert_eq!(player.rating(), 118.89736249706061);
 }
