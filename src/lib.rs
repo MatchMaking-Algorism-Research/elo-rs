881745cc544f59cb5521be7d52851cbb56316de8
Abiria <abiria.dev@gmail.com>
2023-04-07 01:18:13 +0900

    fix: correct formula

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 fn delta(s: f64, e: f64, k: f64) -> f64 { k * (s - e) }
 
 fn ex(ra: f64, rb: f64) -> (f64, f64) {
-	let ea = 10f64.powf((rb - ra) / 400.);
+	let ea = 1. / (1. + 10f64.powf((rb - ra) / 400.));
 
 	(ea, 1. - ea)
 }
@@ -9,5 +9,8 @@ fn ex(ra: f64, rb: f64) -> (f64, f64) {
 pub fn elo(ra: f64, rb: f64, sa: f64, sb: f64, k: f64) -> (f64, f64) {
 	let (ea, eb) = ex(ra, rb);
 
-	(delta(sa, ea, k), delta(sb, eb, k))
+	(
+		ra + delta(sa, ea, k),
+		rb + delta(sb, eb, k),
+	)
 }
